9c75c419f6cc1815372d11ed4f5eb25513e1e042
max5452 <max5452@79f8d254-07d7-56f2-65dd-ad47ee0414cb>
2014-03-12 15:38:24 +0000

    ניסיון מסך מנהל חדש

---
+++ WpfApplication1/MangerMainGui.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace project
+{
+    partial class MangerMainGui
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.exit = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label1.Location = new System.Drawing.Point(177, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ברוך הבא !";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(18, 29);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(189, 34);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "ניהול משתמשי המערכת";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button6);
+            this.groupBox1.Controls.Add(this.button5);
+            this.groupBox1.Controls.Add(this.button4);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Location = new System.Drawing.Point(15, 55);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(502, 207);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "בחר מה ברצונך לעשות";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(18, 105);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(189, 34);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "ניהול עובדי החברה";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // exit
+            // 
+            this.exit.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.exit.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.exit.Location = new System.Drawing.Point(397, 344);
+            this.exit.Name = "exit";
+            this.exit.Size = new System.Drawing.Size(120, 23);
+            this.exit.TabIndex = 3;
+            this.exit.Text = " יציאה מהמערכת";
+            this.exit.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(298, 105);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(189, 34);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "ניהול לקוחות";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(298, 29);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(189, 34);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "ניהול הזמנות";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(163, 158);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(189, 34);
+            this.button6.TabIndex = 4;
+            this.button6.Text = "סטטיסטיקה";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(159, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "שם פרטי של האדם שנכנס";
+            // 
+            // MangerMainGui
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(529, 379);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.exit);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "MangerMainGui";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.RightToLeftLayout = true;
+            this.Text = "מסך ראשי - מנהל";
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button exit;
+        private System.Windows.Forms.Label label2;
+    }
+}
