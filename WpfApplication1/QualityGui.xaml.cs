8a57292fd54a9b36066f83d1e2ea0b1c685761e2
max5452 <max5452@79f8d254-07d7-56f2-65dd-ad47ee0414cb>
2014-03-18 17:48:40 +0000

    שינוי חיבור ל-מחובר או לא מחובר, כולל טבלת SQL מעודכנת שניתן למצוא בתקיית DataBase. הוספת מסך ראשי למזכירה ולאיש האיכות. הוספת מסך משתמשים ומסך עובדים למזכירה ולמנהל האיכות. שינוי רישום תפקיד לעיברית במקום אנגלית. פתיחת מסך ראשי בהתאם לתפקיד. הודעות חדשות במסך ההתחברות. יכול להיות שיש עוד דברים אבל אני לא זוכר כרגע.

---
+++ WpfApplication1/QualityGui.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace project
+{
+    /// <summary>
+    /// Interaction logic for QualityGui.xaml
+    /// </summary>
+    public partial class QualityGui : Window
+    {
+        public QualityGui()
+        {
+            InitializeComponent();
+            this.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+            NameLabel.Content = "                                    שלום " + Login.user_name + "!\n               אנא בחר/י מה ברצונך/ה לעשות.";
+        }
+
+
+    }
+}
