ee1218a178aad11024b5161c473d7d09536f5dbd
max5452 <max5452@79f8d254-07d7-56f2-65dd-ad47ee0414cb>
2014-03-19 11:57:44 +0000

    עדכון קוד שמירת הטבלה לקובץ. עכשיו לא צריך לשנות קידוד אלא רק לפתוח באמצעות אקסל ואז אפשר לשמור את הקובץ כקובץ אקסל תקין.

--- trunk/WpfApplication1/ManagerUsersGui.xaml.cs
+++ trunk/WpfApplication1/ManagerUsersGui.xaml.cs
@@ -24,49 +24,49 @@ namespace project
     /// </summary>
     public partial class ManagerUsersGui : Window
     {
-        public ManagerUsersGui()
-        {
-            InitializeComponent();
-            this.WindowStartupLocation = WindowStartupLocation.CenterScreen;
-         //   ListCollectionView CollectionViewList;
-          //  DataTable DataTableFiltered;
-         //   DataSet ds = new DataSet();
+                public ManagerUsersGui()
+                {
+                    InitializeComponent();
+                    this.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+                 //   ListCollectionView CollectionViewList;
+                  //  DataTable DataTableFiltered;
+                 //   DataSet ds = new DataSet();
 
-            try
-            {
-                string Connectionstring = " Server=localhost;Database=project; UId=root;Password=1234;";
-                MySqlConnection MySqlConn = new MySqlConnection(Connectionstring);
-                MySqlConn.Open();
-                string Query1 = ("select users.empid as `תעודת זהות`,employees.emp_firstname as `שם פרטי` ,employees.emp_lastname as `שם משפחה` ,users.user_name as `שם משתמש` ,password as סיסמה ,role as תפקיד ,connected as מחובר ,email as `כתובת אימייל` from project.users , project.employees where users.empid=employees.empid");
-                MySqlCommand MSQLcrcommand1 = new MySqlCommand(Query1, MySqlConn);
-                MSQLcrcommand1.ExecuteNonQuery();
-                MySqlDataAdapter mysqlDAdp = new MySqlDataAdapter(MSQLcrcommand1);
-                DataTable dt = new DataTable("users");
-                mysqlDAdp.Fill(dt);
-                dataGrid1.ItemsSource = dt.DefaultView; 
+                    try
+                    {
+                        string Connectionstring = " Server=localhost;Database=project; UId=root;Password=1234;";
+                        MySqlConnection MySqlConn = new MySqlConnection(Connectionstring);
+                        MySqlConn.Open();
+                        string Query1 = ("select users.empid as `תעודת זהות`,employees.emp_firstname as `שם פרטי` ,employees.emp_lastname as `שם משפחה` ,users.user_name as `שם משתמש` ,password as סיסמה ,role as תפקיד ,connected as מחובר ,email as `כתובת אימייל` from project.users , project.employees where users.empid=employees.empid");
+                        MySqlCommand MSQLcrcommand1 = new MySqlCommand(Query1, MySqlConn);
+                        MSQLcrcommand1.ExecuteNonQuery();
+                        MySqlDataAdapter mysqlDAdp = new MySqlDataAdapter(MSQLcrcommand1);
+                        DataTable dt = new DataTable("users");
+                        mysqlDAdp.Fill(dt);
+                        dataGrid1.ItemsSource = dt.DefaultView; 
 
-            //    mysqlDAdp.Fill(ds);
-            //    ICollectionView UsersView = CollectionViewSource.GetDefaultView(dt);
-            //    UsersView.Filter
-           //     CollectionViewList = new ListCollectionView(ds);
-                mysqlDAdp.Update(dt);
-                MySqlConn.Close();
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
+                    //    mysqlDAdp.Fill(ds);
+                    //    ICollectionView UsersView = CollectionViewSource.GetDefaultView(dt);
+                    //    UsersView.Filter
+                   //     CollectionViewList = new ListCollectionView(ds);
+                        mysqlDAdp.Update(dt);
+                        MySqlConn.Close();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
 
 
 
-        /*
-            DBConnection conn = new DBConnection();
-            string query = ("select userid as מספר__משתמש ,first_name as שם__פרטי ,last_name as שם__משפחה ,user_name as שם__משתמש ,password as סיסמה ,role as תפקיד ,connected as מחובר ,email as כתובת__אימייל from users");
-            dataGrid1.ItemsSource = conn.GetDataTableFromDB(query).Tables[0].DefaultView;
-            DataTable dt = new DataTable();
+                /*
+                    DBConnection conn = new DBConnection();
+                    string query = ("select userid as מספר__משתמש ,first_name as שם__פרטי ,last_name as שם__משפחה ,user_name as שם__משתמש ,password as סיסמה ,role as תפקיד ,connected as מחובר ,email as כתובת__אימייל from users");
+                    dataGrid1.ItemsSource = conn.GetDataTableFromDB(query).Tables[0].DefaultView;
+                    DataTable dt = new DataTable();
             
-        */
-        }
+                */
+                }
 
 
 
@@ -74,63 +74,47 @@ namespace project
 
 
 
-        private void TXTBtn_Click(object sender, RoutedEventArgs e)
-        {
+            private void TXTBtn_Click(object sender, RoutedEventArgs e)
+                {
            
-            ExportToTXT();
-        }
+                    ExportToTXT();
+                }
 
 
 
 
 
-        private void ExportToTXT()
-        {
-            SaveFileDialog dialog = new SaveFileDialog();
-            dialog.FileName = "רשימת משתמשים"; // Default file name
-            dialog.DefaultExt = ".text"; // Default file extension
-            dialog.Filter = "Text documents (.txt)|*.txt"; // Filter files by extension 
+            private void ExportToTXT()
+                {
+                    SaveFileDialog dialog = new SaveFileDialog();
+                    dialog.FileName = "רשימת משתמשים"; // Default file name
+                    dialog.DefaultExt = ".text"; // Default file extension
+                    dialog.Filter = "Text documents (.txt)|*.txt";  //EXcel documents (.xlsx)|*.xlsx";    // Filter files by extension 
 
-            // Show save file dialog box
-            Nullable<bool> result = dialog.ShowDialog();
+                    // Show save file dialog box
+                    Nullable<bool> result = dialog.ShowDialog();
 
-            // Process save file dialog box results 
-            if (result == true)
-            {
-                dataGrid1.SelectAllCells();
-                dataGrid1.ClipboardCopyMode = DataGridClipboardCopyMode.IncludeHeader;
-                ApplicationCommands.Copy.Execute(null, dataGrid1);
-                String resultat1 = (string)Clipboard.GetData(DataFormats.CommaSeparatedValue);
-                String result1 = (string)Clipboard.GetData(DataFormats.Text);
-                dataGrid1.UnselectAllCells();
-                string saveto = dialog.FileName;
-                System.IO.StreamWriter file = new System.IO.StreamWriter(@saveto);
-                //   System.IO.StreamWriter file = new System.IO.StreamWriter(@"C:\Users\Shuki\Dropbox\Braude\לימודים\Final Pro\Scoend Stage\p\פרוייקט\Excel\test.txt");
-                //   file.WriteLine(result.Replace("s","d");
+                    // Process save file dialog box results 
+                    if (result == true)
+                    {
+                        dataGrid1.SelectAllCells();
+                        dataGrid1.ClipboardCopyMode = DataGridClipboardCopyMode.IncludeHeader;
+                        ApplicationCommands.Copy.Execute(null, dataGrid1);
+                        String result1 = (string)Clipboard.GetData(DataFormats.Text);
+                        dataGrid1.UnselectAllCells();
+                        string saveto = dialog.FileName;
+                        System.IO.StreamWriter file = new System.IO.StreamWriter(@saveto,false,Encoding.Default);
+                
+                        file.WriteLine(result1.Replace("‘,’", "‘ ‘"));
+                        file.Close();
+                        file.Dispose();
+                        // Save document 
+                        MessageBox.Show("                                                                             !קובץ הטקסט נשמר\n\n           :כדי לפתוח באקסל מומלץ להשתמש ב''פתיחה באמצעות'' ולבחור ב\n\n                                                 ''Microsoft Excel''");
+                    }
+                }
 
-                file.WriteLine(result1.Replace("‘,’", "‘ ‘"));
-                file.Close();
 
-                // Save document 
-                string filename = dialog.FileName;
-                MessageBox.Show("Unicode קובץ הטקסט נוצר - להצגה באקסל שמור מחדש בתבנית ");
-            }
-
-     /*   dataGrid1.SelectAllCells();
-        dataGrid1.ClipboardCopyMode = DataGridClipboardCopyMode.IncludeHeader;
-        ApplicationCommands.Copy.Execute(null, dataGrid1);
-        String resultat = (string)Clipboard.GetData(DataFormats.CommaSeparatedValue);
-        String result = (string)Clipboard.GetData(DataFormats.Text);
-        dataGrid1.UnselectAllCells();
-        System.IO.StreamWriter file = new System.IO.StreamWriter(@"C:\Users\Shuki\Dropbox\Braude\לימודים\Final Pro\Scoend Stage\p\פרוייקט\Excel\test.txt");
-     //   file.WriteLine(result.Replace("s","d");
-            
-            file.WriteLine(result.Replace("‘,’", "‘ ‘"));
-        file.Close();
-
-        MessageBox.Show("Unicode קובץ הטקסט נוצר - להצגה באקסל שמור מחדש בתבנית ");
-      */
-        }
+    
 
 
 
