7cc694ee656ca478b15b2f2c125a4a1fccb8b4a9
max5452 <max5452@79f8d254-07d7-56f2-65dd-ad47ee0414cb>
2014-04-15 14:53:57 +0000

    הוספת בחירת תאריך לשדות של התאריך בטבלה במסך העבודות

---
+++ trunk/WpfApplication1/ManagerJobGui.xaml.cs
@@ -0,0 +1,412 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using MySql.Data.MySqlClient;
+using System.Data;
+using System.ComponentModel;
+using Microsoft.Win32;
+using System.Text.RegularExpressions;
+namespace project
+{
+    /// <summary>
+    /// Interaction logic for ManagerJobGui.xaml
+    /// </summary>
+    public partial class ManagerJobGui : Window
+    {
+        public ManagerJobGui()
+        {
+            InitializeComponent();
+            this.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+
+
+            try
+            {
+                MySqlConnection MySqlConn = new MySqlConnection(Login.Connectionstring);
+                MySqlConn.Open();
+                string Query1 = ("select jobid as `מספר עבודה`,costumerid as `מספר לקוח` ,job_status as `סטטוס עבודה`,jobdescription  as `תאור עבודה` ,startDate  as `תאריך התחלה`,expectedFinishDate as `תאריך סיום משוער` ,actualFinishDate as `תאריך סיום בפועל`  from jobs group by jobid");
+                MySqlCommand MSQLcrcommand1 = new MySqlCommand(Query1, MySqlConn);
+                MSQLcrcommand1.ExecuteNonQuery();
+                MySqlDataAdapter mysqlDAdp = new MySqlDataAdapter(MSQLcrcommand1);
+                DataTable dt = new DataTable("jobs");
+                mysqlDAdp.Fill(dt);
+                dataGrid1.ItemsSource = dt.DefaultView;
+                mysqlDAdp.Update(dt);
+                MySqlConn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+
+
+
+        private void TXTBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+            ExportToTXT();
+        }
+
+
+
+
+
+        private void ExportToTXT()
+        {
+            SaveFileDialog dialog = new SaveFileDialog();
+            dialog.FileName = "רשימת לקוחות"; // Default file name
+            dialog.DefaultExt = ".text"; // Default file extension
+            dialog.Filter = "Text documents (.txt)|*.txt";  //EXcel documents (.xlsx)|*.xlsx";    // Filter files by extension 
+
+            // Show save file dialog box
+            Nullable<bool> result = dialog.ShowDialog();
+
+            // Process save file dialog box results 
+            if (result == true)
+            {
+                dataGrid1.SelectAllCells();
+                dataGrid1.ClipboardCopyMode = DataGridClipboardCopyMode.IncludeHeader;
+                ApplicationCommands.Copy.Execute(null, dataGrid1);
+                String result1 = (string)Clipboard.GetData(DataFormats.Text);
+                dataGrid1.UnselectAllCells();
+                string saveto = dialog.FileName;
+                System.IO.StreamWriter file = new System.IO.StreamWriter(@saveto, false, Encoding.Default);
+
+                file.WriteLine(result1.Replace("‘,’", "‘ ‘"));
+                file.Close();
+                file.Dispose();
+                // Save document 
+                MessageBox.Show("                                                                             !קובץ הטקסט נשמר\n\n           :כדי לפתוח באקסל מומלץ להשתמש ב''פתיחה באמצעות'' ולבחור ב\n\n                                                 ''Microsoft Excel''");
+            }
+        }
+
+
+
+        private void CustumerNameSearchTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            try
+            {
+
+                MySqlConnection MySqlConn = new MySqlConnection(Login.Connectionstring);
+                MySqlConn.Open();
+                String searchkey = this.CustumerNameSearchTextBox.Text;
+                string Query1 = "select costumerid as `מספר לקוח`,costumerName as `שם לקוח` ,costumerAddress as `כתובת לקוח`  from project.costumers  where  costumerName Like '%" + searchkey + "%' group by costumerid";
+                MySqlCommand MSQLcrcommand1 = new MySqlCommand(Query1, MySqlConn);
+                MSQLcrcommand1.ExecuteNonQuery();
+                MySqlDataAdapter mysqlDAdp = new MySqlDataAdapter(MSQLcrcommand1);
+                DataTable dt = new DataTable("custumers");
+                mysqlDAdp.Fill(dt);
+                dataGrid1.ItemsSource = dt.DefaultView;
+                mysqlDAdp.Update(dt);
+                MySqlConn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void IDSearchTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            try
+            {
+                MySqlConnection MySqlConn = new MySqlConnection(Login.Connectionstring);
+                MySqlConn.Open();
+                String searchidkey = this.IDSearchTextBox.Text;
+                string Query1 = "select costumerid as `מספר לקוח`,costumerName as `שם לקוח` ,costumerAddress as `כתובת לקוח`  from project.costumers  where  costumerid Like '%" + searchidkey + "%' group by costumerid";
+                MySqlCommand MSQLcrcommand1 = new MySqlCommand(Query1, MySqlConn);
+                MSQLcrcommand1.ExecuteNonQuery();
+                MySqlDataAdapter mysqlDAdp = new MySqlDataAdapter(MSQLcrcommand1);
+                DataTable dt = new DataTable("Custumers");
+                mysqlDAdp.Fill(dt);
+                dataGrid1.ItemsSource = dt.DefaultView;
+                mysqlDAdp.Update(dt);
+                MySqlConn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void ADD_Btn_Click(object sender, RoutedEventArgs e)
+        {
+            ManagerAddNewCusGUI MACG = new ManagerAddNewCusGUI();
+            MACG.Show();
+            this.Close();
+        }
+
+
+
+        private void DeleteBtn_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                System.Collections.IList rows = dataGrid1.SelectedItems;
+                DataRowView row = (DataRowView)dataGrid1.SelectedItems[0];
+                if (MessageBox.Show("?האם אתה בטוח שברצונך למחוק לקוח זה", "וידוא מחיקה", MessageBoxButton.YesNo, MessageBoxImage.Warning) == MessageBoxResult.No)
+                {
+                    //do no stuff
+                }
+                else // if the user clicked on "Yes" so he wants to Delete.
+                {
+                    // this will give us the first colum of the selected row in the DataGrid.
+
+                    string selected = row["מספר לקוח"].ToString();
+                    // MessageBox.Show("" + selected + "");
+
+                    try
+                    {
+                        MySqlConnection MySqlConn = new MySqlConnection(Login.Connectionstring);
+                        MySqlConn.Open();
+                        string Query1 = "delete from costumers where costumerid='" + selected + "'";
+                        MySqlCommand MSQLcrcommand1 = new MySqlCommand(Query1, MySqlConn);
+                        MSQLcrcommand1.ExecuteNonQuery();
+                        MySqlDataAdapter mysqlDAdp = new MySqlDataAdapter(MSQLcrcommand1);
+                        MySqlConn.Close();
+                        MessageBox.Show("!הלקוח נמחק מהמערכת");
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+                    try
+                    {
+                        MySqlConnection MySqlConn = new MySqlConnection(Login.Connectionstring);
+                        MySqlConn.Open();
+                        string Query1 = ("select costumerid as `מספר לקוח`,costumerName as `שם לקוח` ,costumerAddress as `כתובת לקוח`  from project.costumers group by costumerid");
+                        MySqlCommand MSQLcrcommand1 = new MySqlCommand(Query1, MySqlConn);
+                        MSQLcrcommand1.ExecuteNonQuery();
+                        MySqlDataAdapter mysqlDAdp = new MySqlDataAdapter(MSQLcrcommand1);
+                        DataTable dt = new DataTable("custumers");
+                        mysqlDAdp.Fill(dt);
+                        dataGrid1.ItemsSource = dt.DefaultView;
+                        mysqlDAdp.Update(dt);
+                        MySqlConn.Close();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+                }//end else
+
+            }//end try
+            catch { MessageBox.Show("לא נבחר לקוח למחיקה"); }
+
+        }//end function
+
+
+
+        // go to previous screen.
+        private void Back_Btn_Click(object sender, RoutedEventArgs e)
+        {
+            ManagerGui MG = new ManagerGui();
+            MG.Show();
+            this.Close();
+        }
+
+        private void UpdateBtn_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                System.Collections.IList rows = dataGrid1.SelectedItems;
+
+                DataRowView row = (DataRowView)dataGrid1.SelectedItems[0];
+                string selected = row["מספר לקוח"].ToString();
+                // MessageBox.Show(""+selected+ "");
+
+
+
+                if (MessageBox.Show("?האם אתה בטוח שברצונך לעדכן לקוח זה", "וידוא עדכון", MessageBoxButton.YesNo, MessageBoxImage.Warning) == MessageBoxResult.No)
+                {
+                    //dont do stuff
+                }
+                else // if the user clicked on "Yes" so he wants to Update.
+                {
+                    string custumername = row["שם לקוח"].ToString();
+                    string custumeraddress = row["כתובת לקוח"].ToString();
+
+                    try
+                    {
+
+                        MySqlConnection MySqlConn = new MySqlConnection(Login.Connectionstring);
+                        MySqlConn.Open();
+                        string Query1 = "update costumers set costumerName='" + custumername + "',costumerAddress='" + custumeraddress + "' where costumerid='" + selected + "'";
+                        MySqlCommand MSQLcrcommand1 = new MySqlCommand(Query1, MySqlConn);
+                        MSQLcrcommand1.ExecuteNonQuery();
+                        MySqlDataAdapter mysqlDAdp = new MySqlDataAdapter(MSQLcrcommand1);
+                        MySqlConn.Close();
+                        MessageBox.Show("!פרטי הלקוח עודכנו");
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+                    try
+                    {
+                        MySqlConnection MySqlConn = new MySqlConnection(Login.Connectionstring);
+                        MySqlConn.Open();
+                        string Query1 = ("select costumerid as `מספר לקוח`,costumerName as `שם לקוח` ,costumerAddress as `כתובת לקוח`  from project.costumers group by costumerid");
+                        MySqlCommand MSQLcrcommand1 = new MySqlCommand(Query1, MySqlConn);
+                        MSQLcrcommand1.ExecuteNonQuery();
+                        MySqlDataAdapter mysqlDAdp = new MySqlDataAdapter(MSQLcrcommand1);
+                        DataTable dt = new DataTable("custumers");
+                        mysqlDAdp.Fill(dt);
+                        dataGrid1.ItemsSource = dt.DefaultView;
+                        mysqlDAdp.Update(dt);
+                        MySqlConn.Close();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+
+                }
+
+            }
+            catch { MessageBox.Show("לא נבחר לקוח לעדכון "); }
+        }
+
+
+
+        private void Grid_AutoGeneratingColumn(object sender, DataGridAutoGeneratingColumnEventArgs e)
+        {
+            if (e.Column.Header.ToString() == "מספר לקוח")
+            {
+                // e.Cancel = true;   // For not to include 
+                e.Column.IsReadOnly = true; // Makes the column as read only
+            }
+
+            //if the Column is a date Column then show me only the date
+            if (e.Column.Header.ToString() == "תאריך התחלה")
+            {
+                (e.Column as DataGridTextColumn).Binding.StringFormat = "dd/MM/yyyy";
+
+                /*
+                // Create a new template column.
+                DataGridTemplateColumn templateColumn = new DataGridTemplateColumn();
+                templateColumn.CellTemplate = (DataTemplate)Resources["dueDateCellTemplate"];
+                templateColumn.CellEditingTemplate = (DataTemplate)Resources["dueDateCellEditingTemplate"];
+                e.Column = templateColumn;
+                */
+
+                DataGridTemplateColumn dgct = new DataGridTemplateColumn();
+                dgct.Header = "תאריך התחלה";
+                dgct.SortMemberPath = "תאריך התחלה";
+
+                Binding b = new Binding("תאריך התחלה");
+                b.StringFormat = "dd/MM/yyyy";
+
+                #region Editing
+                FrameworkElementFactory factory = new FrameworkElementFactory(typeof(DatePicker));
+                factory.SetValue(DatePicker.SelectedDateProperty, b);
+                DataTemplate cellEditingTemplate = new DataTemplate();
+                cellEditingTemplate.VisualTree = factory;
+                dgct.CellEditingTemplate = cellEditingTemplate;
+                #endregion
+
+                #region View
+                FrameworkElementFactory sfactory = new FrameworkElementFactory(typeof(TextBlock));
+                sfactory.SetValue(TextBlock.TextProperty, b);
+                DataTemplate cellTemplate = new DataTemplate();
+                cellTemplate.VisualTree = sfactory;
+                dgct.CellTemplate = cellTemplate;
+                #endregion
+                e.Column = dgct;
+            }
+
+            if (e.Column.Header.ToString() == "תאריך סיום משוער" )
+            {
+                DataGridTemplateColumn dgct = new DataGridTemplateColumn();
+                dgct.Header = "תאריך סיום משוער";
+                dgct.SortMemberPath = "תאריך סיום משוער";
+
+                Binding b = new Binding("תאריך סיום משוער");
+                b.StringFormat = "dd/MM/yyyy";
+
+                #region Editing
+                FrameworkElementFactory factory = new FrameworkElementFactory(typeof(DatePicker));
+                factory.SetValue(DatePicker.SelectedDateProperty, b);
+                DataTemplate cellEditingTemplate = new DataTemplate();
+                cellEditingTemplate.VisualTree = factory;
+                dgct.CellEditingTemplate = cellEditingTemplate;
+                #endregion
+
+                #region View
+                FrameworkElementFactory sfactory = new FrameworkElementFactory(typeof(TextBlock));
+                sfactory.SetValue(TextBlock.TextProperty, b);
+                DataTemplate cellTemplate = new DataTemplate();
+                cellTemplate.VisualTree = sfactory;
+                dgct.CellTemplate = cellTemplate;
+                #endregion
+                e.Column = dgct;
+            }
+
+            if (e.Column.Header.ToString() == "תאריך סיום בפועל")
+            {
+                DataGridTemplateColumn dgct = new DataGridTemplateColumn();
+                dgct.Header = "תאריך סיום בפועל";
+                dgct.SortMemberPath = "תאריך סיום בפועל";
+
+                Binding b = new Binding("תאריך סיום בפועל");
+                b.StringFormat = "dd/MM/yyyy";
+
+                //#region Editing
+                FrameworkElementFactory factory = new FrameworkElementFactory(typeof(DatePicker));
+                factory.SetValue(DatePicker.SelectedDateProperty, b);
+                DataTemplate cellEditingTemplate = new DataTemplate();
+                cellEditingTemplate.VisualTree = factory;
+                dgct.CellEditingTemplate = cellEditingTemplate;
+                //#endregion
+
+                #region View
+                FrameworkElementFactory sfactory = new FrameworkElementFactory(typeof(TextBlock));
+                sfactory.SetValue(TextBlock.TextProperty, b);
+                DataTemplate cellTemplate = new DataTemplate();
+                cellTemplate.VisualTree = sfactory;
+                dgct.CellTemplate = cellTemplate;
+                #endregion
+                e.Column = dgct;
+            }
+        }
+
+        private void Contacts_button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                DataRowView row = (DataRowView)dataGrid1.SelectedItems[0];
+                string selected = row["מספר לקוח"].ToString();
+                string CosName = row["שם לקוח"].ToString();
+                string cosADDs = row["כתובת לקוח"].ToString();
+                ManagerContactsGUI MCG = new ManagerContactsGUI(selected, CosName, cosADDs);
+                MCG.Show();
+                this.Close();
+            }
+            catch { MessageBox.Show("לא נבחר לקוח"); }
+        }
+
+
+
+
+
+    }
+
+}
+
+
+
+
+
+
+
+      
